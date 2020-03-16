a3169bd77e2478ecbe8c448a7096fbd3f3808ef8
zinwang <32264884+zinwang@users.noreply.github.com>
2020-03-16 22:43:59 +0800

    Add files via upload

--- roomframe.py
+++ roomframe.py
@@ -7,41 +7,45 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QFont
 
 class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(598, 406)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(10, 320, 391, 31))
-        self.lineEdit.setObjectName("lineEdit")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(420, 320, 81, 31))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(470, 10, 91, 31))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(10, 10, 421, 301))
-        self.textEdit.setObjectName("textEdit")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 598, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+	def setupUi(self, MainWindow):
+		MainWindow.setObjectName("MainWindow")
+		MainWindow.resize(598, 406)
+		self.centralwidget = QtWidgets.QWidget(MainWindow)
+		self.centralwidget.setObjectName("centralwidget")
+		self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+		self.lineEdit.setGeometry(QtCore.QRect(10, 320, 391, 31))
+		self.lineEdit.setObjectName("lineEdit")
+		self.lineEdit.setFont(QFont("Arial",12,50))
+		self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+		self.pushButton.setGeometry(QtCore.QRect(420, 320, 81, 31))
+		self.pushButton.setObjectName("pushButton")
+		self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+		self.pushButton_2.setGeometry(QtCore.QRect(470, 10, 91, 31))
+		self.pushButton_2.setObjectName("pushButton_2")
+		self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+		self.textEdit.setGeometry(QtCore.QRect(10, 10, 421, 301))
+		self.textEdit.setObjectName("textEdit")
+		MainWindow.setCentralWidget(self.centralwidget)
+		self.menubar = QtWidgets.QMenuBar(MainWindow)
+		self.menubar.setGeometry(QtCore.QRect(0, 0, 598, 21))
+		self.menubar.setObjectName("menubar")
+		MainWindow.setMenuBar(self.menubar)
+		self.statusbar = QtWidgets.QStatusBar(MainWindow)
+		self.statusbar.setObjectName("statusbar")
+		MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+		self.retranslateUi(MainWindow)
+		QtCore.QMetaObject.connectSlotsByName(MainWindow)
         
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "ClientRoom"))
-        self.pushButton.setText(_translate("MainWindow", "Send"))
-        self.pushButton_2.setText(_translate("MainWindow", "Leave"))
+	def retranslateUi(self, MainWindow):
+		_translate = QtCore.QCoreApplication.translate
+		MainWindow.setWindowTitle(_translate("MainWindow", "ClientRoom"))
+		self.pushButton.setFont(QFont("Arial",10,70))
+		self.pushButton.setText(_translate("MainWindow", "Send"))
+		self.pushButton_2.setFont(QFont("Arial",10,70))
+		self.pushButton_2.setText(_translate("MainWindow", "Leave"))
 
